7941db6e4e9f77673224f7860b9491b7b7ceafff
trudyvianai <107434432+trudyvianai@users.noreply.github.com>
2022-07-12 07:37:58 -0700

    fixing api routes - checking console log in function log

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(_req, res) {
+  console.log("🟣 does this even work to console log");
   // Get data from your database
   res.status(200).json({ test: "heyyy" });
 }
